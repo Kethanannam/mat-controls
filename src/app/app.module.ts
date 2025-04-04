86e21631082358da94f8020a907044e2cf491925
Manohar <manohar.a@calibrage.in>
2025-04-04 12:08:59 +0530

    commit from1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';  // Correct imports
 import { FormsModule } from '@angular/forms';
 
 
